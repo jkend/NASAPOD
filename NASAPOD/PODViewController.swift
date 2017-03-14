962a1ab0bb597bdbba45557852071911a79fb373
jkend <jkend@users.noreply.github.com>
2017-03-14 17:20:37 -0400

    added logic for downloading, setting of properties

--- NASAPOD/PODViewController.swift
+++ NASAPOD/PODViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
-class PODViewController: UIViewController {
+class PODViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var imageScrollView: UIScrollView!
+    @IBOutlet weak var imageScrollView: UIScrollView! {
+        didSet {
+            imageScrollView.contentSize = podImageView.frame.size
+        }
+    }
 
-    @IBOutlet weak var podImageView: UIImageView!
-    var imageURL: String?
+    private var podImageView = UIImageView()
+    
+    var apodImage: UIImage? {
+        get {
+            return podImageView.image
+        }
+        set {
+            podImageView.image = newValue
+            podImageView.sizeToFit()
+            imageScrollView?.contentSize = podImageView.frame.size
+        }
+    }
+    
+    var imageURL: String? {
+        didSet {
+            podImageView.image = nil
+            if view.window != nil {
+                downloadImage()
+            }
+        }
+    }
     var imageTitle: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageScrollView.addSubview(podImageView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if (apodImage == nil) {
+            downloadImage()
+        }
+    }
+    
+    
+    private func downloadImage() {
+        guard let imageURL = imageURL else {
+            return
+        }
+        if let url = URL(string: imageURL) {
+            DispatchQueue.global(qos: .userInitiated).async {
+                guard imageURL == url.absoluteString else {
+                    return
+                }
+                let dataFromUrl = NSData(contentsOf: url)
+                DispatchQueue.main.async { [weak weakSelf = self] in
+                    if let imageData = dataFromUrl {
+                        let apodImage = UIImage(data: imageData as Data)
+                        weakSelf?.apodImage = apodImage
+                    }
+                }
+
+            }
+            
+        }
+        
+    }
 
 }
 
