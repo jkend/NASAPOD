8cf66f7f90bde255794605ab135c55974df1ce51
jkend <jkend@users.noreply.github.com>
2017-03-14 15:46:00 -0400

    added new file

--- NASAPOD/APOD.swift
+++ NASAPOD/APOD.swift
@@ -2,65 +2,18 @@
 //  APOD.swift
 //  NASAPOD
 //
-//  Created by Joy Kendall on 3/13/17.
+//  Created by Joy Kendall on 3/14/17.
 //  Copyright © 2017 Joy. All rights reserved.
 //
 
 import Foundation
 
 class APOD {
-    private lazy var dateFormatter: DateFormatter = {
-        let df = DateFormatter()
-        df.locale = Locale(identifier: "en_US_POSIX")
-        df.dateFormat = "yyyy-MM-dd"
-        return df
-    }()
-    
-    private struct Constants {
-        static let ApiKey: String = "DEMO_KEY"
-        static let BaseUrl: String = "https://api.nasa.gov/planetary/apod?api_key="
-    }
-    
-    func getTodaysPOD() {
-        let url = String(format: "%@%@", Constants.BaseUrl, Constants.ApiKey)
-        getPODJson(url)
-    }
-    
-    func getPastPOD(fromDate: Date) {
-        let dateString = dateFormatter.string(from: fromDate)
-        let url = String(format: "%@%@&date=%@", Constants.BaseUrl, Constants.ApiKey, dateString)
-        print(url)
-        getPODJson(url)
-  
-    }
-    
-    private func getPODJson(_ urlString: String) {
-        let requestURL = URL(string: urlString)
-        let urlRequest = URLRequest(url: requestURL!)
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: urlRequest) { (data, response, error) in
-            
-            if let error = error {
-                print(error)
-            }
-            guard let responseData = data else {
-                print("Error: No data")
-                return
-            }
-            do {
-                guard let apodData = try JSONSerialization.jsonObject(with: responseData, options: [])
-                    as? [String: Any] else {
-                        print("error trying to convert data to JSON")
-                        return
-                }
-                print(apodData)
-            } catch  {
-                print("error trying to convert data to JSON")
-                return
-            }
-        }
-        task.resume()
-    }
+    var title: String?
+    var detailText: String?
+    var imageURL: String?
+    var hdImageURL: String?
+    var author: String?
+    var errorMessage: String?
     
 }
